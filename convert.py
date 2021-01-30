3865e7f07045bbd1b961270c1f88c650006abda7
Nurul-GC <Nurul-GC@github.com>
2021-01-30 20:58:18 +0000

    atualizando o commit

--- convert.py
+++ convert.py
@@ -1,43 +1,65 @@
-import pdfcrowd 
+import pdfcrowd
 import os
 import shutil
 import platform
 from time import sleep
-if("windows" in platform.system().lower()):
+import colorama
+
+# ativando o colorama, ferramenta que
+# facilita a coloração do terminal
+colorama.init(autoreset=True)
+
+if "windows" in platform.system().lower():
     c = 'cls'
 else:
     c = 'clear'
-os.system(c)
-dir = './arquivosConversor'
-def creatdir(dir):
-    if os.path.isdir(dir):
-         print('\033[1;32mVERIFIQUE A PASTA arquivosConversor PARA ENCONTRAR OS FICHEIROS\033[m') 
-         sleep(3)
-         os.system(c)
+# os.system(c)
+diretorio = './arquivosConvertidos'
+
+
+def creatdir(nome_dir: str):
+    if os.path.isdir(nome_dir):
+        print(f'{colorama.Fore.GREEN}OS ARQUIVOS SERÃO GUARDADOS EM "arquivosConvertidos"\nVERIFIQUE A PASTA NO FINAL DA OPERAÇÃO PARA ENCONTRAR OS FICHEIROS..')
+        sleep(5)
+        # os.system(c)
     else:
-        os.mkdir(dir)
-        print('\033[1;32mVERIFIQUE A PASTA arquivosConversor PARA ENCONTRAR OS FICHEIROS\033[m')
-        sleep(3)
-        os.system(c)
-creatdir(dir)  
+        os.mkdir(nome_dir)
+        print(f'{colorama.Fore.GREEN}OS ARQUIVOS SERÃO GUARDADOS EM "arquivosConvertidos"\nVERIFIQUE A PASTA NO FINAL DA OPERAÇÃO PARA ENCONTRAR OS FICHEIROS..')
+        sleep(5)
+        # os.system(c)
+
+
 def banner():
-    print('-'*30)
-    print('CONVERSOR HTML TO PDF 1.0') 
-    print('-'*30) 
-def main(site, nome):
+    print(f"""{colorama.Fore.CYAN}
+\t\t--------------------------------
+\t\t|  CONVERSOR HTML TO PDF v1.0  |
+\t\t--------------------------------
+""")
+
+
+def main(endereco_site, nome_arquivo):
+    print(f'\n[*] POR FAVOR AGUARDE, O ARQUIVO {nome_arquivo}.pdf ESTA A SER REPRODUZIDO!')
     api = pdfcrowd.HtmlToPdfClient("demo", "ce544b6ea52a5621fb9d55f8b542d14d")
     try:
-        if site not in 'https://':
-            api.convertUrlToFile('https://'+site, nome+'.pdf')
+        if endereco_site not in 'https://':
+            api.convertUrlToFile('https://' + endereco_site, f'{nome_arquivo}.pdf')
         else:
-            api.convertUrlToFile(site, nome+'.pdf')
-        shutil.move(nome+'.pdf',dir) 
-        print('-'*30)
-        print('\033[1;32mCONVERSÃO FEITA COM SUCESSO\033[m') 
-    except:
-        print('\033[1;31m\nACONTECEU UM ERRO NÃO PROGRAMÁVEL!\nTENTE NOVAMENTE OU VERIFIQUE A SUA CONEXÃO A INTERNET\033[m')
-banner()
-site = str(input('DIGITE A URL DO SITE: '))
-nome = str(input('DIGITE O NOME QUE QUER NO PDF: '))
-main(site, nome)
-print('\n\033[1;33mBy: SÍLVIO SILVA \033[m') 
+            api.convertUrlToFile(endereco_site, f'{nome_arquivo}.pdf')
+        shutil.move(f'{nome_arquivo}.pdf', diretorio)
+        print('-' * 30)
+        print(f'{colorama.Fore.GREEN}CONVERSÃO FEITA COM SUCESSO')
+    except Exception as e:
+        print(f'\n{colorama.Fore.RED}ACONTECEU UM ERRO NÃO PROGRAMÁVEL!\nTENTE NOVAMENTE OU VERIFIQUE A SUA CONEXÃO A INTERNET..')
+
+
+if __name__ == '__main__':
+    try:
+        creatdir(diretorio)
+        banner()
+        site = str(input('DIGITE A URL DO SITE:\n> '))
+        nome = str(input('DIGITE O NOME COM QUE QUER GUARDAR O PDF:\n> '))
+        main(site, nome)
+    except Exception as erro:
+        pass
+    finally:
+        print(f'\n{colorama.Fore.YELLOW}By: SÍLVIO SILVA')
